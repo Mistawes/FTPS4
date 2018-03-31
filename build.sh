90d0c7fcda6d56ea2170bd6e288c583ac8986f84
Mistawes <wes.moore@gmail.com>
2018-03-31 19:25:08 +0100

    Updated build/clean scripts..

--- build.sh
+++ build.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-set -e
-
-pushd tool
-make
-popd
-
-make
-
-tool/bin2js ps4-ftp-vtx.bin > exploit/payload.js
+make clean && make
--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pushd tool
-make clean
-popd
-
-make clean
-
-rm -f ps4-ftp-vtx.bin
